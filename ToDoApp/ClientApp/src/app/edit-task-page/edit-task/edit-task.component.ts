339c4835784140c70a01d5f88b5906be97c513e8
Jana Kazakova <kazakovayana95@icloud.com>
2021-04-09 18:34:01 +0300

    add sass to project

--- ToDoApp/ClientApp/src/app/edit-task-page/edit-task/edit-task.component.ts
+++ ToDoApp/ClientApp/src/app/edit-task-page/edit-task/edit-task.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.component.html',
-  styleUrls: ['./edit-task.component.css']
+  styleUrls: ['./edit-task.component.scss']
 })
 export class EditTaskComponent implements OnInit {
 
