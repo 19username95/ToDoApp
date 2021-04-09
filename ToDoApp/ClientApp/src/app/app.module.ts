ff443e1d2e3078084ecc3f56b3c794258e7fde3b
Jana Kazakova <kazakovayana95@icloud.com>
2021-04-09 17:35:10 +0300

    remove default components
    from app module

--- ToDoApp/ClientApp/src/app/app.module.ts
+++ ToDoApp/ClientApp/src/app/app.module.ts
@@ -3,26 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
-    FetchDataComponent
+    AppComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ]),
+    RouterModule.forRoot([]),
     AppRoutingModule
   ],
   providers: [],
