a66b0ea75232b9f0b2e46d5d6f06cf96d59c4b0f
Jana Kazakova <kazakovayana95@icloud.com>
2021-04-10 14:08:50 +0300

    config db connection and add migrations

---
+++ ToDoApp/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoApp.Models
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
--- ToDoApp/Startup.cs
+++ ToDoApp/Startup.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ToDoApp.Models;
 
 namespace ToDoApp
 {
@@ -25,6 +26,11 @@ namespace ToDoApp
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
+            });
+
+            services.AddDbContext<TaskContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionString:DevConnection"]);
             });
         }
 
